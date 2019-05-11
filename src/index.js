630add4338acaa244d372b0056ba445d879b9055
Steve Kedzie <seal.team.smyhk@gmail.com>
2019-05-11 10:05:30 -0700

    Setup basic WebSockets

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketio = require('socket.io');
 
-const server = express();
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 const port = process.env.PORT || 3000;
-
 const publicDirectoryPath = path.join(__dirname, '../public');
-server.use(express.static(publicDirectoryPath));
+
+app.use(express.static(publicDirectoryPath));
+
+io.on('connection', () => {
+  console.info('New WebSocket connection');
+});
 
 server.listen(port, err => {
   if (err) console.error('Server could not start');
