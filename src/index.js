c511cda2e2967fffc13f2e6726cd755bb374c5d2
Steve Kedzie <seal.team.smyhk@gmail.com>
2019-05-11 09:32:33 -0700

    Setup public directory

--- src/index.js
+++ src/index.js
@@ -2,15 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const server = express();
-
 const port = process.env.PORT || 3000;
+
 const publicDirectoryPath = path.join(__dirname, '../public');
-
 server.use(express.static(publicDirectoryPath));
-
-server.get('/', (req, res) => {
-  res.send('Chat App');
-});
 
 server.listen(port, err => {
   if (err) console.error('Server could not start');
