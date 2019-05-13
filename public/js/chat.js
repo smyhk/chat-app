1ed0561f5c2b31aa7b9d5e1f2a2b7b3e74c8d609
Steve Kedzie <seal.team.smyhk@gmail.com>
2019-05-13 09:14:14 -0700

    Ensure basic sockets are working

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,19 @@
-io();
+// client
+const socket = io();
+
+socket.on('message', msg => {
+  console.log(msg);
+});
+
+// socket.on('countUpdated', count => {
+//   console.log('count updated', count);
+// });
+
+document.querySelector('#message-form').addEventListener('submit', e => {
+  e.preventDefault();
+
+  const message = e.target.elements.message.value;
+  // document.querySelector('message').value;
+
+  socket.emit('sendMessage', message);
+});
